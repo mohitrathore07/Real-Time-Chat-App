84f1ddd3402a6839c06cc30d3dbe46645dcbf4cb
Mohit Rathore <mohitrathore8269@gmail.com>
2025-02-24 21:15:04 +0530

    project completed

---
+++ frontend/src/Pages/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useChatStore } from '../store/useChatStore'
+import Sidebar from '../Componants/Sidebar';
+import NoChatSelected from '../Componants/NoChatSelected';
+import ChatContainer from '../Componants/ChatContainer';
+
+
+const HomePage = () => {
+  const {selectedUser} = useChatStore();
+  return (
+    <div className='h-screen bg-base-200'>
+      <div className='flex flex-items justify-center pt-20 px-4'>
+        <div className='bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]'>
+          <div className='flex h-full rounded-lg overflow-hidden'>
+            <Sidebar />
+            {
+              !selectedUser ? <NoChatSelected /> : <ChatContainer />
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
